55621d4c2c0a7b4d39b64c6c168d239e54e63939
Phillip Scholz <zyanklee@gmail.com>
2012-10-23 19:27:58 +0200

    Fixed if clauses

--- autoripper.sh
+++ autoripper.sh
@@ -41,9 +41,9 @@ CDDBPROTO=6
 
 
 # is cddb info valid and has cd info
-function check_cddb_info {
-	cddbinfocheck=0
-}
+#function check_cddb_info {
+#	cddbinfocheck=0
+#}
 
 
 # eject cd
@@ -55,18 +55,16 @@ function do_eject {
 # check which audio cd this is and how many tracks it has
 function get_cddb_info {
 
-	DISCID=$( ${DISCIDTOOL} )
-	CDDBRESPONSE=$( ${CDDBTOOL} query "${CDDBURL}" ${CDDBPROTO} ${USER} ${HOSTNAME} ${DISCID} )
+	DISCID="$( ${DISCIDTOOL} $CDROM )"
+ 	CDDBRESPONSE="$( ${CDDBTOOL} query ${CDDBURL} ${CDDBPROTO} ${USER} ${HOSTNAME} ${DISCID} | tail -n+2 | head -n 1 )"
 
-	check_cddb_info()
-
-	if ( "${cddbinfocheck}" == "0" ) {
-		ARTISTALBUM="$(echo ${CDDBRESPONSE} | head -n 2 | tail -n 1 | cut -d ' ' -f 3- | sed 's/\//-/g')"
-		TRACKS="echo $DISCID | cut -d ' ' -f 2)"
-	}
-	else {
-		do_eject()
-	}
+	if [ $? == 0 ]
+	then
+		ARTISTALBUM="$(echo ${CDDBRESPONSE} | cut -d ' ' -f 3- | sed 's/\//-/g')"
+		TRACKS="$( echo $DISCID | cut -d ' ' -f 2 )"
+	else
+		do_eject
+	fi
 
 }
 
@@ -75,21 +73,23 @@ function check_local {
 	# is there a directory for this artist/album combination?
 	echo "${DESTINATION}/${ARTISTALBUM}"
 
-	if ( -d "${DESTINATION}/${ARTISTALBUM}") { # album directory exists
-
+	if [ -d "${DESTINATION}/${ARTISTALBUM}" ] # album directory exists
+	then
 		# get number of audio-files in this dir
 		CNT=$(find "${DESTINATION}/${ARTISTALBUM}" -type f -name '*.flac' | wc -l)
 
-		if ( $CNT < $TRACKS) { # files in dir is lower than tracks on disc
+		if [ $CNT < $TRACKS ] # files in dir is lower than tracks on disc
+		then
 			# => return incomplete
-		}
-		else {
+			LOCALCHECK="incomplete"
+		else
 			# => return complete
-		}
-	}
-	else { # new album
+			LOCALCHECK="complete"
+		fi
+	else # new album
 		# => return newalbum
-	}
+		LOCALCHECK="newalbum"
+	fi
 }
 
 
@@ -97,9 +97,10 @@ function check_local {
 function select_tracks {
 	for i in `seq -w 1 $TRACKS` # check if each number ...
 	do
-		if ( ! -f "${DESTINATION}/${ARTISTALBUM}/${i}\ *" ) { # ... is there and if not ...
+		if ( ! -f "${DESTINATION}/${ARTISTALBUM}/${i}\ *" ) # ... is there and if not ...
+		then
 			selected+=( "${i}" ) # ... add to list of selected tracks
-		}
+		fi
 	done
 }
 
