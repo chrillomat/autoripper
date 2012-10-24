32986fa82ce65bec674a03a82dd274f2c4e496f7
Phillip Scholz <zyanklee@gmail.com>
2012-10-24 22:19:02 +0200

    added updatexbmc.py and changed paths to $MYDIR

--- abcde.sh
+++ abcde.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-python xbmc-notify.py "Started ripping $7"
+$MYDIR="/usr/local/autoripper"
+
+$MYDIR/xbmc-notify.py "Started ripping $7"
 OUTPUTDIR=$1 WAVOUTPUTDIR=${1}/tmp/ OUTPUTTYPE=$2 $3 -c $4/abcde.conf ${8}
 chown -R ${5}: "${6}/${7}"
-python xbmc-notify.py "Finished ripping $7"
+$MYDIR/updatexbmc.py
+$MYDIR/xbmc-notify.py "Finished ripping $7"
